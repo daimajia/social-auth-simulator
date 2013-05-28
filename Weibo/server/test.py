06761ba9ef72ba20b4dc8180a7fde1c9369dc590
xuanqinanhai <xuanqinanhai@gmail.com>
2013-05-28 20:07:49 +0800

    修改了文件结构

--- Weibo/server/test.py
+++ Weibo/server/test.py
@@ -25,4 +25,4 @@ def auth(uid,password,app_key,app_secret,callback_uri):
 
 
 if __name__ == '__main__':
-	auth("你的微博用户名","你的微博密码","3471849585","7b0dd7f9bae77257734586afcfbafd38","https://api.weibo.com/oauth2/default.html")
+	auth("你的微博用户名","你的博密码","3471849585","7b0dd7f9ba微e77257734586afcfbafd38","https://api.weibo.com/oauth2/default.html")
