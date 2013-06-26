e9b90b167c4fdf2c31779d816f41b1a48a21b054
xuanqinanhai <xuanqinanhai@gmail.com>
2013-06-26 12:11:27 +0800

    fix weibo bug

--- Weibo/local/test.py
+++ Weibo/local/test.py
@@ -1,5 +1,12 @@
+# -*- coding: utf8 -*-
 from weibo import WeiboAutoAuth
 
-api = WeiboAutoAuth('你的app_key','你的app_secret','你的appredirect_uri','微博用户名','微博密码')
+app_key = '3530915833' #请修改成您的app_key
+app_secret = 'f34a9eb3404c7f99b5e8466e18ce9b6e' #请修改成您的app_secret
+redirect_uri = 'http://snsapi.sinaapp.com/auth.php' #请修改成您的redirect_uri
+username = '' #请修改成您的测试用户名
+password = '' #请修改成您的测试微博密码
+
+api = WeiboAutoAuth(app_key,app_secret,redirect_uri,username,password)
 
 print api.get_access_token()
--- Weibo/local/weibo.py
+++ Weibo/local/weibo.py
@@ -1,6 +1,9 @@
 # -*- coding: utf8 -*-
 import urllib
 import urllib2
+import cookielib
+import requests
+from bs4 import BeautifulSoup
 
 class WeiboAutoAuth(object):
 	'''
@@ -19,31 +22,59 @@ class WeiboAutoAuth(object):
 		return base_url % (self.app_key,self.redirect_uri)
 
 	def get_code(self):
-		postdata = {"client_id": self.app_key,
-			"redirect_uri": self.redirect_uri,
-			"userId": self.username,
-			"passwd": self.password,
-			"isLoginSina": "0",
-			"action": "submit",
-			"response_type": "code"
-		}
-		auth_url = 'https://api.weibo.com/oauth2/authorize'
-		referer_url = self.__get_authorize_url()
-		headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0",
+
+		print self.__get_authorize_url()
+		# step1: 打开登陆框，并且记录cookies
+
+		open_login_form = requests.get(self.__get_authorize_url())
+		open_login_form_html = open_login_form.text
+		open_login_form_url = open_login_form.url
+
+		soup = BeautifulSoup(open_login_form_html)
+		inputs = soup.find_all('input')
+
+		#step2:获取表单数据
+		login_form_data = {}
+		for input in inputs:
+			value = input.get('value')
+			login_form_data[input.get('name')] = unicode(value).encode('utf-8')
+		
+		login_form_data['userId'] = self.username
+		login_form_data['passwd'] = self.password
+
+		print login_form_data
+
+		#step3:提交表单
+		headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0",
 			"Host": "api.weibo.com",
-			"Referer": referer_url
+			"Referer":self.__get_authorize_url(),
+			"Origin":"http://api.weibo.com"
 		}
-		request = urllib2.Request(
-			url = auth_url,
-			data = urllib.urlencode(postdata),
-			headers = headers
-		)
-		try:
-			response = urllib2.urlopen(request)
-			code = response.geturl()[-32:]
-			return code
-		except Exception as e:
-			print "Error:",e
+
+		auth_url = 'https://api.weibo.com/oauth2/authorize'
+		response = requests.post(auth_url,data = login_form_data,headers=headers)
+		url = response.url
+
+		#如果已经认证过，则地址中包含code=，然后直接返回
+		if('code=' in url):
+			return url[-32:]
+
+		content = response.text
+
+		soup = BeautifulSoup(content)
+		auth_data = {}
+		inputs = soup.find_all('input')
+
+		for input in inputs:
+			value = input.get('value')
+			auth_data[input.get('name')] = unicode(value).encode('utf-8')
+
+		# print auth_data
+
+		sure_url = 'https://api.weibo.com/2/oauth2/authorize'
+		r =  requests.post(sure_url,data = auth_data, headers = headers)
+		return r.url[-32:]
 
 	def get_access_token(self):
 		postdata = {
@@ -54,8 +85,6 @@ class WeiboAutoAuth(object):
 			'client_secret':self.app_secret
 		}
 		request_url = 'https://api.weibo.com/oauth2/access_token'
-		request = urllib2.Request(
-			url = request_url,
-			data = urllib.urlencode(postdata)
-		)
-		return urllib2.urlopen(request).read()
+
+		r = requests.post(request_url,data=postdata)
+		return r.text
